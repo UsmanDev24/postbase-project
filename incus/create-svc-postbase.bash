f8b817c530fb84aa8b954d4399703eef4b2dfaed
Usman Ghani <usman.dev24@gmail.com>
2026-05-01 17:34:14 +0500

    fix: fix in create-svc-postbase

--- incus/create-svc-postbase.bash
+++ incus/create-svc-postbase.bash
@@ -13,7 +13,7 @@ incus launch images:debian/13 $CONTAINER_NAME -c limits.cpu=1 -c limits.memory=1
 
 #incus exec $CONTAINER_NAME -- su - admin -c 'sudo apt update'
 
-incus exec $CONTAINER_NAME -- bash -c "
+incus exec $CONTAINER_NAME -- bash << 'EOF'
   apt-get update && \
   apt-get install -y git unzip curl && \
 
@@ -22,13 +22,14 @@ incus exec $CONTAINER_NAME -- bash -c "
   npm install --global corepack@latest && \
   
   curl -fsSL https://bun.sh/install | bash - && \
-  source /root/.bashrc 
+  source /root/.bashrc
   bun --help 
   nodejs -v
   npm -v 
-  
-  npm install pm2@latest -g -d
-"
+
+  npm install pm2@latest -g 
+  pm2 -v
+EOF
 
 
 insus exec $CONTAINER_NAME -- curl -fsSL https://dotenvx.sh | sh -
@@ -41,32 +42,34 @@ read -p "Enter your DOTENV_PRIVATE_KEY_PRODUCTION: " MY_KEY_PRODUCTION && \
 incus config set $CONTAINER_NAME environment.DOTENV_PRIVATE_KEY_PRODUCTION="$MY_KEY_PRODUCTION" && \
 unset MY_KEY_PRODUCTION
 
-incus exec $CONTAINER_NAME -- bash -c "
+incus exec $CONTAINER_NAME -- bash << 'EOF'
   cd ~ 
   rm -r ./postbase
 
   git clone https://github.com/UsmanDev24/postbase.git && \
-  
   cd ./postbase && \
 
+  source /root/.bashrc
   bun install && \
   bun run build && \
   bun run init-catg-pro && \
 
   pm2 start --name postbase bun -- start
-"
-incus exec $CONTAINER_NAME -- bash -c "
+EOF
+
+incus exec $CONTAINER_NAME -- bash << 'EOF'
   cd ~ 
   rm -r ./postbase-user-api
   git clone https://github.com/UsmanDev24/postbase-user-api.git && \
   
+  source /root/.bashrc
   cd ./postbase-user-api && \
   bun install && \
   bun run build && \
   pm2 start --name postbase-user-api bun -- start && \
   eval $(pm2 startup | grep ^sudo) && \
   pm2 save
-"
+EOF
 
   
 
